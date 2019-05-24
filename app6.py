0c758054c57c5b77b89d53e30ca335aba5ff6d6b
Rachel Poole <rachelpoole@hotmail.com>
2019-05-25 08:24:18 +1000

    latest changes

--- app6.py
+++ app6.py
@@ -8,6 +8,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+internal_stylesheets = './assets/rachel_style.css'
 
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 
@@ -23,40 +24,45 @@ df = (df.copy()
         .reset_index()
         .assign(cumulative_annual = lambda x: x.groupby(x['date'].dt.year)['amount'].cumsum().multiply(-1),
                 cumulative_month = lambda x: x.groupby(x['date'].dt.to_period('M'))['amount'].cumsum().multiply(-1),
-                month = lambda x: x['date'].dt.to_period('M').astype('str'), 
+                month = lambda x: x['date'].dt.to_period('M').dt.strftime('%b %Y'), 
                 day = lambda x: x['date'].dt.day))
 
 # General app structure
 ## Add titles etc in here
 app.layout = html.Div([
     html.Div([
+
+        html.Div([
+            dcc.Graph(id='months-graphic'),
+        ],  
+        style={"height" : "75%", 
+               "width" : "50%"}),
+
         html.Div([
             dcc.Dropdown(id='yaxis-column-1',
                          options=[{'label': i, 'value': i} for i in df['month'].unique()], 
-                         value='2019-04'
-                         ),
+                         value='Apr 2019'),
         ],
-        style={'width': '20%', 
-               'display': 'inline-block'}),
+        style={'width': '17%', 
+               'display': 'inline-block', 
+               'margin-left': '100px'}),
 
         html.Div([
             dcc.Dropdown(
                 id='yaxis-column-2',
                 options=[{'label': i, 'value': i} for i in df['month'].unique()],
-                value='2019-05'),
+                value='Mar 2019'),
         ],
-        style={'width': '20%', 
-               'float': 'left', 
+        style={'width': '17%', 
+               'margin-left': '20px', 
                'display': 'inline-block'})
     ]),
-
-    dcc.Graph(id='months-graphic'),
 
 ])
 
 # Formatting properties
 labels = ['This month to date', 'Last month', 'Average month']
-colours = ['rgb(98, 29, 75)', 'rgb(61, 72, 73)', 'rgb(189,189,189)']
+colours = ['rgb(98, 29, 75)', 'rgb(61, 72, 73)', 'rgb(0, 0, 0)' ]
 mode_size = [12, 8, 8]
 line_size = [4, 2, 2]
 
@@ -67,6 +73,17 @@ line_size = [4, 2, 2]
 
 def update_graph(yaxis_column_1_name, yaxis_column_2_name):
     traces = []
+    # # range(2) because there are two dropdowns
+    # for i in range(2):
+    #     traces.append(go.Scatter(
+    #             x = df.loc[df['month'] == i, 'day'],
+    #             y = df.loc[df['month'] == i, 'cumulative_month'],
+    #             name = "{}".format(i),
+    #             mode = 'lines', 
+    #             line = dict(color = 'rgb(98, 29, 75)', 
+    #                         width = line_size[0]),
+    #             connectgaps=True))
+    
     traces.append(go.Scatter(
                 x = df.loc[df['month'] == yaxis_column_1_name, 'day'],
                 y = df.loc[df['month'] == yaxis_column_1_name, 'cumulative_month'],
@@ -74,7 +91,8 @@ def update_graph(yaxis_column_1_name, yaxis_column_2_name):
                 mode = 'lines', 
                 line = dict(color = colours[0], 
                             width = line_size[0]),
-                connectgaps=True))
+                connectgaps=True))    
+
     traces.append(go.Scatter(
                 x = df.loc[df['month'] == yaxis_column_2_name, 'day'],
                 y = df.loc[df['month'] == yaxis_column_2_name, 'cumulative_month'],
@@ -84,6 +102,23 @@ def update_graph(yaxis_column_1_name, yaxis_column_2_name):
                             width = line_size[1]),
                 connectgaps=True))
 
+    traces.append(go.Scatter(
+                x = [df.loc[df['month'] == yaxis_column_1_name, 'day'].max()], 
+                y = [df.loc[df['month'] == yaxis_column_1_name, 'cumulative_month'].max()],
+                name = "{}".format(yaxis_column_1_name),
+                mode = 'markers', 
+                marker = dict(color = colours[0], 
+                              size = mode_size[0]),
+                hoverinfo='skip'))    
+
+    traces.append(go.Scatter(
+                x = [df.loc[df['month'] == yaxis_column_2_name, 'day'].max()], 
+                y = [df.loc[df['month'] == yaxis_column_2_name, 'cumulative_month'].max()],
+                name = "{}".format(yaxis_column_2_name),
+                mode = 'markers', 
+                marker = dict(color = colours[1], 
+                              size = mode_size[1]),
+                hoverinfo='skip'))    
 
     layout = go.Layout(xaxis = dict(showline=True,
                                     showgrid=False,
@@ -128,7 +163,7 @@ def update_graph(yaxis_column_1_name, yaxis_column_2_name):
                             y=1.2,
                             xanchor='left', 
                             yanchor='bottom',
-                            text='Hi Rachel',
+                            text='Hi, Rachel',
                             font=dict(family='Arial',
                                       size=30,
                                       color='rgb(61, 72, 73)'),
@@ -143,14 +178,14 @@ def update_graph(yaxis_column_1_name, yaxis_column_2_name):
                             yanchor='bottom',
                             text='How is your spend tracking this month?',
                             font=dict(family='Arial',
-                                      size=14,
+                                      size=16,
                                       color='rgb(61, 72, 73)'),
                             showarrow=False))
     # Note
     annotations.append(dict(xref='paper', 
                             yref='paper', 
                             x=0, 
-                            y=-0.2,
+                            y=-0.15,
                             xanchor='left',
                             yanchor='top',
                             text='Note: All data is fabricated. This project was created in 2019.',
@@ -159,33 +194,56 @@ def update_graph(yaxis_column_1_name, yaxis_column_2_name):
                                       color='rgb(150,150,150)'),
                                       showarrow=False))
 
-# # # Line labels
-# for i in range(len(labels)):
-#     annotations.append(dict(xref='paper', 
-#                             x=0.95, 
-#                             y=report_data[report_data.columns[i]].max(),
-#                             xanchor='left', 
-#                             yanchor='middle',
-#                             text='${0:,.2f}'.format(report_data[report_data.columns[i]].max()),
-#                             font=dict(family='Arial',
-#                                       size=16, 
-#                                       color = colours[i]),
-#                             showarrow=False))
-    
-# for i in range(len(labels)):
-#     annotations.append(dict(xref='paper', 
-#                             x=0.95, 
-#                             y=report_data[report_data.columns[i]].max() + 1000,
-# #                             y=report_data[report_data.columns[i]].max() + (report_data.max().max() * 0.1),
-#                             xanchor='left', 
-#                             yanchor='middle',
-#                             text=labels[i],
-#                             font=dict(family='Arial',
-#                                       size=12, 
-#                                       color = colours[i]),
-#                             showarrow=False))
+    # Dropdowns 
+    annotations.append(dict(xref='paper', 
+                            yref='paper', 
+                            x= 0.0, 
+                            y=-0.3,
+                            xanchor='left',
+                            yanchor='top',
+                            text='Select month',
+                            font=dict(family='Arial',
+                                      size=14,
+                                      color='rgb(61,72,73)'),
+                                      showarrow=False))
 
-    layout['annotations'] = annotations # This adds the annotations to the layout dictionary - otherwise gets a bit unwieldy
+
+    # Dropdowns 
+    annotations.append(dict(xref='paper', 
+                            yref='paper', 
+                            x= 0.53, 
+                            y=-0.3,
+                            xanchor='left',
+                            yanchor='top',
+                            text='Select comparison month',
+                            font=dict(family='Arial',
+                                      size=14,
+                                      color='rgb(61,72,73)'),
+                                      showarrow=False))
+
+    annotations.append(dict(xref='paper', 
+                            x = (df.loc[df['month'] == yaxis_column_1_name, 'day'].max()/max(df.loc[df['month'] == yaxis_column_1_name, 'day'].max(), df.loc[df['month'] == yaxis_column_2_name, 'day'].max()))* 0.97, 
+                            y=df.loc[df['month'] == yaxis_column_1_name, 'cumulative_month'].max(), 
+                            xanchor='left', 
+                            yanchor='middle',
+                            text= "{}".format(yaxis_column_1_name),
+                            font=dict(family='Arial',
+                                      size=16, 
+                                      color = 'rgb(98, 29, 75)'),
+                            showarrow=False))
+
+    annotations.append(dict(xref='paper', 
+                            x = (df.loc[df['month'] == yaxis_column_2_name, 'day'].max()/max(df.loc[df['month'] == yaxis_column_2_name, 'day'].max(), df.loc[df['month'] == yaxis_column_1_name, 'day'].max())) * 0.97, 
+                            y=df.loc[df['month'] == yaxis_column_2_name, 'cumulative_month'].max(), 
+                            xanchor='left', 
+                            yanchor='middle',
+                            text= "{}".format(yaxis_column_2_name),
+                            font=dict(family='Arial',
+                                      size=16, 
+                                      color = 'rgb(61,72,73)'),
+                            showarrow=False))
+
+    layout['annotations'] = annotations # This adds the annotations to the layout dictionary 
 
     return {
         'data': traces,
